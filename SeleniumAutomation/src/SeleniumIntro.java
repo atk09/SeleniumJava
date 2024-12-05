74095ae7844c9ee7de69b2fde5f28a2c4250c71b
atk09 <ayeshakhan56@gmail.com>
2024-12-05 16:23:36 +0000

    first commit

---
+++ SeleniumAutomation/src/SeleniumIntro.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// ChromeDriver, FirefoxDriver etc all would implement WebDriver interface to use
+// WebDriver methods
+public class SeleniumIntro {
+
+	public static void main(String[] args) {
+		
+		// Chrome -> Chrome driver -> close methods
+		// WebDriver methods + class methods 
+		// driver object has knowledge of WebDriver implementation of ChromeDriver
+		
+	
+		// first get the chromedriver.exe that invokes chrome browser
+		// webdriver.chrome.driver -> value of path of chromedriver.exe
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Documents\\API+Concepts+Testing\\SeleniumAutomation\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+		System.out.println(driver.getTitle());
+		
+		driver.close();  // to close only one tab window
+		driver.quit();   // to quit all associated windows
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
