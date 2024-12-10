03b0ebea3d8d76552f31083497c47cf52b39c24c
atk09 <ayeshakhan56@gmail.com>
2024-12-10 14:09:47 +0000

    updating main file with different drivers

--- SeleniumAutomation/src/SeleniumIntro.java
+++ SeleniumAutomation/src/SeleniumIntro.java
@@ -14,20 +14,22 @@ public class SeleniumIntro {
 	
 		// first get the chromedriver.exe that invokes chrome browser
 		// webdriver.chrome.driver -> value of path of chromedriver.exe
-		
+		// gecko.driver is for firefox browser
+		// edge.driver is for msedgedriver
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Documents\\API+Concepts+Testing\\SeleniumAutomation\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		
 		driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
 		System.out.println(driver.getTitle());
 		
-		driver.close();  // to close only one tab window
+		driver.close();  // to close only one current tab window
 		driver.quit();   // to quit all associated windows
 		
 		
 		
 		
 		
-		
+		 
 
 	}
 
